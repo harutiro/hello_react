6d7f3f791acfe4493fffedf278c73ff44c8873ce
harutiro <harutiro2027@outlook.jp>
2022-12-02 08:41:47 +0900

    update:useEffectなどを使ってみる

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { ColoredMessage } from "./components/ColoredMessage";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const App = () => {
 
+    console.log("レンダリング");
+
+    const [num , setNum] = useState(0);
+
     const onClickButton = () => {
-        alert();
+        // alert();
+        // 直前の内容を取得する。
+        //こうしないとコンポーネントがレンダリングする前に変更があると期待した動作をしない。
+        setNum((prev) => prev + 1)
     };
+
+    // const [変数名, 変数を更新する関数] = useState(初期値);
+    // ある値が変わった時に、ある動作をする。
+    // 第二引数を[]と設定すると、最初の一回だけ実行される。APIなどの取得に使える。
+    useEffect(() => {
+        console.log("useEffect");
+        // 2秒後に実行される
+    }, [num]);
 
     return (
         <>
@@ -19,6 +36,8 @@ export const App = () => {
             <ColoredMessage color="pink" >元気です！</ColoredMessage>
 
             <button onClick={onClickButton}>ぼたん</button>
+
+            <p>{num}</p>
         </>
     );
 };
