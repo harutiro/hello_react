f84104a999b2045fd1c10075ccdc3fd1de94c3e8
harutiro <harutiro2027@outlook.jp>
2022-12-01 18:34:38 +0900

    update:コンポーネントを使ってみる。

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+
 export const App = () => {
 
     const onClickButton = () => {
         alert();
     };
 
+    const contentPinkStyle = {
+        color: "pink",
+        fontSize: "20px"
+    }
+
     return (
         <>
             {/* ジャバスクリプトの処理をそのまま書くことができる。 */}
             {console.log("hoge")}
-            <h1>Hello World</h1>
-            <p>お元気ですか？</p>
+
+            {/* Javascriptのオブジェクトとして定義をする。 */}
+            <h1 style={{color: "red"}} >Hello World</h1>
+
+            {/* コンポーネントを用いて定義をする。 */}
+            <ColoredMessage/>
+
+            <p style={contentPinkStyle}>元気です！</p>
             <button onClick={onClickButton}>ぼたん</button>
         </>
     );
---
+++ src/components/ColoredMessage.jsx
@@ -0,0 +1,8 @@
+export const ColoredMessage = () => {
+    // {/* オブジェクトを定義して、それを読み込むこともできる */}
+    const contentStyle = {
+        color: "blue",
+        fontSize: "20px"
+    }
+    return <p style={contentStyle}>お元気ですか？</p>
+}
