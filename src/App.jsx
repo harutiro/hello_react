c2b51e3d0e0b441503dcaaa5e730395b64bcb9da
harutiro <harutiro2027@outlook.jp>
2022-12-01 18:14:27 +0900

    update: ボタンを作った。

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 export const App = () => {
+
+    const onClickButton = () => {
+        alert();
+    };
+
     return (
         <>
+            {/* ジャバスクリプトの処理をそのまま書くことができる。 */}
+            {console.log("hoge")}
             <h1>Hello World</h1>
             <p>お元気ですか？</p>
+            <button onClick={onClickButton}>ぼたん</button>
         </>
     );
 };
